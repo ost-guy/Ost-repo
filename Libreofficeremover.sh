2e63974ec02febda510803e675ef3ade572fefd4
Ost <bel.isabellyramospessoal@gmail.com>
2026-03-04 19:06:24 -0300

    Add files via upload
    Signed-off-by: Ost <bel.isabellyramospessoal@gmail.com>

---
+++ Libreofficeremover.sh
@@ -0,0 +1,5 @@
+sudo apt purge libreoffice-common -y 
+echo if apt dont work will be used pacman if you use fedora rpm-ostree and dnf will be used
+sudo pacman -Rns libreoffice-fresh libreoffice-still --noconfirm 2>/dev/null
+sudo dnf remove libreoffice-core -y 2>/dev/null
+sudo rpm-ostree override remove libreoffice-core 2>/dev/null
